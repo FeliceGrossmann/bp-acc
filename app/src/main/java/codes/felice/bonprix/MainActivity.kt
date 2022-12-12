f5c2b364b41311fe3096604038d5112e291def0d
Felice Grossmann <felice.g@me.com>
2022-12-12 20:07:51 +0100

    * added navigation animations

--- app/src/main/java/codes/felice/bonprix/MainActivity.kt
+++ app/src/main/java/codes/felice/bonprix/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -25,6 +26,7 @@ class MainActivity : ComponentActivity() {
 
     private val viewModel: BonViewModel by viewModels()
 
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
--- app/src/main/java/codes/felice/bonprix/navigation/NavigationController.kt
+++ app/src/main/java/codes/felice/bonprix/navigation/NavigationController.kt
@@ -1,19 +1,23 @@
 package codes.felice.bonprix.navigation
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import codes.felice.bonprix.models.BonViewModel
 import codes.felice.bonprix.ui.screen.CategoryScreen
 import codes.felice.bonprix.ui.screen.OverviewScreen
 import codes.felice.bonprix.ui.screen.WebViewScreen
+import com.google.accompanist.navigation.animation.AnimatedNavHost
+import com.google.accompanist.navigation.animation.composable
+import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 
+@ExperimentalAnimationApi
 @Composable
 fun NavigationController(viewModel: BonViewModel) {
 
-    val navController = rememberNavController()
+    val navController = rememberAnimatedNavController()
     viewModel.navController = navController
 
     BackHandler(enabled = true) {
@@ -22,17 +26,41 @@ fun NavigationController(viewModel: BonViewModel) {
         navController.popBackStack()
     }
 
-    NavHost(navController = navController, startDestination = Screen.Overview.route) {
+    AnimatedNavHost(navController = navController, startDestination = Screen.Overview.route) {
 
-        composable(route = Screen.Overview.route) {
+        composable(
+            route = Screen.Overview.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentScope.SlideDirection.Start,
+                    animationSpec = tween(700)
+                )
+            }
+        ) {
             OverviewScreen(viewModel = viewModel)
         }
 
-        composable(route = Screen.Category.route) {
+        composable(
+            route = Screen.Category.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentScope.SlideDirection.Start,
+                    animationSpec = tween(700)
+                )
+            }
+        ) {
             CategoryScreen(viewModel = viewModel)
         }
 
-        composable(route = Screen.WebView.route) {
+        composable(
+            route = Screen.WebView.route,
+            enterTransition = {
+                slideIntoContainer(
+                    towards = AnimatedContentScope.SlideDirection.Start,
+                    animationSpec = tween(700)
+                )
+            }
+        ) {
             WebViewScreen(viewModel = viewModel)
         }
 
